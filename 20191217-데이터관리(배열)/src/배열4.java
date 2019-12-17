4bb5562aaf60261a57b4ba2c441f5cebe0220fef
sist187 <sist187@DESKTOP-60AHNQT>
2019-12-17 17:55:00 +0900

    배열4

---
+++ 20191217-데이터관리(배열)/src/배열4.java
@@ -0,0 +1,27 @@
+/*
+ * 		관련된 내용을 묶어서 사용
+ * 
+ * 		=> 같은 데이터형 묶어서 한개 이름으로 제어 : 배열
+ * 		   ========
+ * 		   int[] arr={'A','B'...} ==> arr[0],arr[1],arr[2]
+ * 		   int[] arr=new int[3];
+ * 
+ * 			
+ * 		   double[] arr={10,20,30...}
+ * 		   String[] arr={"","","","",""...}  => 클래스 (값이 없는 경우:null)
+ * 
+ * 		   
+ */
+public class 배열4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="aaaaa";
+		String b=null;
+		String c="";
+		a.length();
+		b.length();
+		c.length();
+	}
+
+}
