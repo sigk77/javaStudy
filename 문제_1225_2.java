8b40b77996108d6fb86dff48ea2bc0f0c3831403
Yonghee KIM <58684450+sigk77@users.noreply.github.com>
2019-12-26 01:29:13 +0900

    Add files via upload

---
+++ 문제_1225_2.java
@@ -0,0 +1,17 @@
+/*
+ * 코드에 정의된 변수들을 구분해서 적기
+ * 
+ * 클래스변수(static변수) : int width , int height , 
+ * 인스턴트변수 : int kind , int num , PlayingCard
+ * 지역변수 : kind = k , num = n
+ * 
+ */
+public class 문제_1225_2 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
