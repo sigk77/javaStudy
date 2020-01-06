a2387b96ceac4bea2baee5c081b94469fbbf5915
sigk77 <sigk77@naver.com>
2020-01-06 11:26:08 +0900

    20200106

--- 20200106-객체지향(상속-재사용)/src/MainsClass.java
+++ 20200106-객체지향(상속-재사용)/src/MainsClass.java
@@ -74,12 +74,63 @@ import java.util.*;
 public class MainsClass {
 
 	public static void main(String[] args) {
+		
+		class Human
+		{
+			String name;
+			int age;
+			
+			public Human() {
+				System.out.println("Human() Call...");
+			}
+		}
+		
+		/* 
+		 *	상위 클래스(상속을 내리는 클래스) : super클래스, 부모클래스, Base클래스
+		 *	 하위 클래스(상속을 받는 클래스) : sub클래스 , 자식클래스, 파생클래스
+		 *
+		 *	=> 기존 클래스의 모든 내용을 가지고 와서 확장하는 내용
+		 *	=> 자바의 클래스는 Object상속을 가지고 있다 (Object: 자바의 최상위 클래스)
+		 *	=> static,생성자는 상속을 할 수 없다
+		 *	=> 목적은 중복된 코딩을 방지
+		 *	       재사용을 목적으로 만든 기능
+		 *
+		 *	=> 상속을 내리는 클래스가 항상 크다
+		 */
+		
+		
+		class Sawon extends Human
+		{
+			/*
+			 * String name;
+			 * int age;
+			 * 
+			 */
+			String dept;
+			String loc;
+			
+			public Sawon() {
+				System.out.println("Sawon() Call...");
+			}
+		}
+		
 		// TODO Auto-generated method stub
 		ArrayList<String> list=new ArrayList<String>();    
 		// 꺽새 보는순간 데이터형을 다 바꾸는(제네릭스) 했다고 생각하면 됌
 		list.add("홍길동");
 		
 		String name=(String)list.get(0);
+		
+		Sawon s=new Sawon();
+		s.age=30;
+		s.name="홍길동";
+		s.loc="서울";
+		s.dept="개발부:";
+		
+		System.out.println("이름"+s.name);
+		System.out.println("나이"+s.age);
+		System.out.println("이름"+s.loc);
+		System.out.println("이름"+s.dept);
 
 	}
 
